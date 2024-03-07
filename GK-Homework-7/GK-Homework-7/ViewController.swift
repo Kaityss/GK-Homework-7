479cd231e88a57d85444bb2cdf2077722c17edb1
giacatano <142597978+giacatano@users.noreply.github.com>
2024-03-07 09:40:55 +0200

    Created App

---
+++ GK-Homework-7/GK-Homework-7/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GK-Homework-7
+//
+//  Created by Gia Catano on 2024/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var hiLabel: UILabel!
+    
+    @IBAction func presMeButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
